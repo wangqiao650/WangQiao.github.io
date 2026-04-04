5d0d3b4ac3915ea15da0503c3db408d9e08b0b45
wangqiao650 <wangqiao650@gmail.com>
2026-04-04 20:28:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/WangQiao/',
+    base: '/wangqiao650/',
     const env = loadEnv(mode, '.', '');
     return {
       server: {
