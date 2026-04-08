cebcc10a8d1cc4e64f73cc0be338339af85a172a
wangqiao650 <wangqiao650@gmail.com>
2026-04-08 17:26:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // 加载环境变量
   const env = loadEnv(mode, '.', '');
 
   return {
-    // 统一使用相对路径 './'，这样无论你的仓库叫什么名字都能正常运行
-    base: './', 
+    // 建议修改为 '/'，这最符合个人主页仓库 (User Site) 的部署逻辑
+    base: '/', 
     
     server: {
       port: 3000,
