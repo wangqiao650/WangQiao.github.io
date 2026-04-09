724b1d4f5ac102af4a2225bbddf8c09c77f44202
wangqiao650 <wangqiao650@gmail.com>
2026-04-10 05:11:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // 顶级域名仓库必须用 '/'，如果是二级路径则用 './'
-  // 鉴于你之前的情况，我们先用 '/' 配合 Actions 部署
   base: '/', 
   plugins: [react()],
   build: {
