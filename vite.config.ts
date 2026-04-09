2d03397fcd8001712fc01878bcff7a0cef47d07b
wangqiao650 <wangqiao650@gmail.com>
2026-04-10 04:36:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', 
+  // 使用相对路径，解决所有域名/子路径的 404 问题
+  base: './', 
+  
   plugins: [react()],
+  
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    // 强制不使用 assets 子文件夹，直接把混淆后的 JS/CSS 放在根目录
+    assetsDir: '', 
     rollupOptions: {
       input: path.resolve(__dirname, 'index.html'),
     },
   },
+  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
