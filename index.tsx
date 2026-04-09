5d30c980de9710feec9417b7ce57d386f20c35cd
wangqiao650 <wangqiao650@gmail.com>
2026-04-10 06:12:20 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<h1>Hello, I am Working!</h1>);
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
 }
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
