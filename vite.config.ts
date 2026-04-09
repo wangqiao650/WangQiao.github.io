20db5a5806add55775a037bdc32e39ed31b3e495
wangqiao650 <wangqiao650@gmail.com>
2026-04-10 06:16:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // 必须有这一行
 
 export default defineConfig({
   base: '/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true, // 确保每次清理旧的乱码文件名
-  },
   resolve: {
     alias: {
-      // 这一行最关键：它把 @ 符号锁定在你的项目根目录
-      '@': path.resolve(__dirname, './'), 
-    }
-  }
+      // 这里的 '@' 符号如果你在代码里没用到，可以先删掉或者这样写：
+      '@': '/', 
+    },
+  },
 });
